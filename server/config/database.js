4da1a6996bb3b6ec8ff53a98353ce3cb02549a08
Shujatullah <syedshujatullah29@gmail.com>
2025-11-25 11:44:08 +0530

    feat: Add Sequelize database configuration and a new `start` script to `package.json`.

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3307,
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     logging: false,
-  }
-);
+  })
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
+      dialect: 'mysql',
+      logging: false,
+    }
+  );
 
 module.exports = sequelize;
